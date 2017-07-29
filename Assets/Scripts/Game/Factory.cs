c5eae87b34c4ae88821b3300a82c08fccb200b5e
zanagi <zanagiys@gmail.com>
2017-07-29 19:16:37 +0300

    Onclick raycast on machines

--- Assets/Scripts/Game/Factory.cs
+++ Assets/Scripts/Game/Factory.cs
@@ -6,10 +6,15 @@ public class Factory : MonoBehaviour
     [SerializeField]
     private GameObject machineContainer;
     private Machine[] machines;
-    private float powerConsumption;
+
+    [SerializeField]
+    private float powerConsumption; // Power consumption of factory itself set in editor
+
+    // Mouse input
+    private Vector3 previousClickPosition = Vector3.one * -1.0f;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
         machines = machineContainer.GetComponentsInChildren<Machine>();
 
@@ -18,8 +23,37 @@ public class Factory : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (!GameManager.Instance.Idle)
+            return;
+
         GameManager.Instance.battery.UpdatePower(powerConsumption);
+    }
+
+    private void LateUpdate()
+    {
+        if (!GameManager.Instance.Idle)
+            return;
+
+        if(InputHandler.Instance.Clicked)
+        {
+            // Raycast to see if machine/robot clicked
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(InputHandler.Instance.TouchPosition), out hit, 100.0f))
+            {
+                Machine machine = hit.transform.GetComponent<Machine>();
+                if(machine)
+                {
+                    Debug.Log("Clicked: " + machine.name);
+                }
+            }
+        }
+    }
+
+    public void TurnOffMachine(Machine machine)
+    {
+
     }
 }
