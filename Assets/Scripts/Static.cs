8e3de0b0c8dd48c4034820abe41fc57c45c02756
zanagi <zanagiys@gmail.com>
2017-07-29 16:37:43 +0300

    Static helper class

---
+++ Assets/Scripts/Static.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Static
+{
+    public static string ReplaceAt(this string input, int index, char newChar)
+    {
+        char[] chars = input.ToCharArray();
+        chars[index] = newChar;
+        return new string(chars);
+    }
+}
