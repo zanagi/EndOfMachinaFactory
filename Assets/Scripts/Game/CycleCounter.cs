567e78ecacff4683c7a00c0c7d58cc4c240c72e0
zanagi <zanagiys@gmail.com>
2017-07-29 14:16:26 +0300

    Cycle counter and battery ui

---
+++ Assets/Scripts/Game/CycleCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CycleCounter : MonoBehaviour {
+
+    [SerializeField]
+    private Image cycleMeter;
+
+    [SerializeField]
+    private Text cycleCountText;
+    
+    [SerializeField]
+    private float cycleLength;
+
+    [SerializeField]
+    private int cycleSpeed = 1;
+
+    private float currentCycleTime;
+    private int cycleCount;
+    
+	// Update is called once per frame
+	void FixedUpdate () {
+        currentCycleTime += Time.fixedDeltaTime * cycleSpeed;
+        
+        if(currentCycleTime >= cycleLength)
+        {
+            currentCycleTime -= cycleLength;
+            cycleCount += 1;
+            cycleCountText.text = cycleCount.ToString();
+        }
+        cycleMeter.fillAmount = currentCycleTime / cycleLength;
+	}
+}
